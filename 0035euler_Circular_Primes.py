9776d39ca27068743c770c478408e8f3d24f444f
csaden <chris.saden@udacity.com>
2013-07-30 20:15:26 -0700

    circular primes

--- 0035euler_Circular_Primes.py
+++ 0035euler_Circular_Primes.py
@@ -1,20 +1,37 @@
+
+# The number, 197, is called a circular prime because all rotations of the
+# digits: 197, 971, and 719, are themselves prime.
+# 
+# There are thirteen such primes below 100:
+# 2, 3, 5, 7, 11, 13, 17, 31, 37, 71, 73, 79, and 97.
+# 
+# How many circular primes are there below one million?
+
+from itertools import permutations, combinations
+
 def isCircularPrime(n):
-	global primes
-	for p in permutationsOfDigits(n):
-		if p not in primes: return False
+	for p in n:
+		if not isPrime(p):
+			return False
 	return True
 
-def permutationsOfDigits(n): 197 179 791 719 971 917
-	permutations = []
+def circularPermutations(n):
 	number = str(n)
-	l = len(number) l = 3
-	digits = [digit for digit in number]
-	
-	return permutations
+	perms = []
+	for i in range(0, len(number)):
+		perms.append(int(number))
+		number = number[1:] + number[0]
+	return perms
+
+print circularPermutations(123)
+print circularPermutations(4567)
 
 def isPrime(n):
 	global primes
-	if n in primes: return True
+	if n in primes:
+		return True
+	else:
+		return False
 
 def sundaram3(max_n):
     numbers = range(3, max_n+1, 2)
@@ -29,4 +46,16 @@ def sundaram3(max_n):
         if initial > half:
             return [2] + filter(None, numbers)
 
-primes = sundaram3[]
+primes = sundaram3(1000000)
+circularPrimes = [2]
+print len(primes)
+
+for p in primes:
+	p = str(p)
+	if ('2' in p or '4' in p or '6' in p or '8' in p or '0' in p):
+		continue
+	if isCircularPrime(circularPermutations(p)):
+		circularPrimes.append(int(p))
+
+print circularPrimes
+print len(circularPrimes)
