25297a5e4d9253b7c5356aeb6740893894d9815a
csaden <chris.saden@udacity.com>
2013-08-03 10:59:07 -0700

    coin sums

--- 0031euler_CoinSums.py
+++ 0031euler_CoinSums.py
@@ -1,5 +1,5 @@
 # In England the currency is made up of pound, L, and pence, p, and there are eight coins in general circulation:
-# 
+# DO BETTER
 # 1p, 2p, 5p, 10p, 20p, 50p, L1 (100p) and L2 (200p).
 # It is possible to make L2 in the following way:
 # 
@@ -13,9 +13,10 @@ for a in range(0, 201):
 		for c in range(0, 41):
 			for d in range(0, 21):
 				for e in range(0, 11):
-					for f in range(0, 3):
+					for f in range(0, 5):
 						for g in range(0, 2):
-							if (a*.01 + b*.02 + c*.05 + d*.1 + e*.5 + f*1 + g*2 == 2):
-								solutions.append((a, b, c, d, e, f, g))
+								if (a*.01 + b*.02 + c*.05 + d*.1 + e*.2 + f*.5 + g*1 == 2):
+									print (a,b,c,d,e,f,g,0)
+									solutions.append((a, b, c, d, e, f, g, 0))
 
 print len(solutions)
